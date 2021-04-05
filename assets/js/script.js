457aac96b5878ee24b9820f393404399cb733411
jakelw96 <jake.lw96@gmail.com>
2021-04-04 20:14:20 -0600

    Did quiz header

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,15 @@
 var $timer = document.getElementById('timer');
-
+var timeLeft = 10;
 
 // Start time for quiz that will count down from 60
 function timer() {
-  timeLeft = 60;
-
   var timeInt = setInterval(function() {
       if (timeLeft > 1) {
           $timer.textContent = "Time: " + timeLeft;
           timeLeft--;
+      } else {
+          $timer.textContent = "Time is up!";
+          clearInterval(timeInt);
       }
   }, 1000)
 
