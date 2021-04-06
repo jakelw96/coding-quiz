ab296e957d68b842b8e08690e7a0c78505ec9da8
jakelw96 <jake.lw96@gmail.com>
2021-04-06 00:51:53 -0600

    started creating quiz questions within an array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var $timer = document.getElementById('timer');
 var timeLeft = 10;
 
+
 // Start time for quiz that will count down from 60
 function timer() {
   var timeInt = setInterval(function() {
@@ -14,3 +15,60 @@ function timer() {
   }, 1000)
 
 }
+
+function startQuiz() {
+    var quizQuestions = [
+      {
+        question: "What is the term used for when water vapor is converted back into liquid water?",
+        answers: {
+           a: "Evaporation",
+           b: "Condensation",
+           c: "Precipitation"
+        },
+        correctAnswer: "b"
+      },
+      {
+        question: "What destructive storm forms over the ocean and then makes landfall in coastal states?",
+        answers: {
+           a: "Hurricane",
+           b: "Tornado",
+           c: "Squall"
+        },
+        correctAnswer: "a"
+      },
+      {
+        question: "What is the type of storm that spawns a funnel shaped cloud and very strong winds?",
+        answers: {
+           a: "Snow storm",
+           b: "Dust-devil",
+           c: "Tornado"
+        },
+        correctAnswer: "c"
+      },
+      {
+        question: "What type of cloud is most common and generally doesn't bring rain?",
+        answers: {
+           a: "Cumulus",
+           b: "Cirrus",
+           c: "Stratus"
+        },
+        correctAnswer: "a"
+      },
+      {
+        question: "What type of destructive storm is generally formed in the winter and usually brings high winds and white-out conditions?",
+        answers: {
+           a: "Squall",
+           b: "blizzard",
+           c: "monsoon"
+        },
+        correctAnswer: "b"
+      },
+      {
+          
+      }    
+    ]
+}
+
+
+
+document.getElementById("btn").addEventListener("click", startQuiz);
