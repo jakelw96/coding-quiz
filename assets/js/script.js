ef2d6e6386bc8dc7d4a1044496345a89acf7c012
jakelw96 <jake.lw96@gmail.com>
2021-04-08 23:29:32 -0600

    Worked on adding functions for each question. Working on iterating through a loop. Updated CSS styling and added buttons for answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,65 +1,121 @@
 var $timer = document.getElementById('timer');
 var timeLeft = 60;
+var $currentQuestion = document.getElementById('question');
+var $startBtn = document.getElementById('btn');
+var score = 0; 
+var $ans1 = document.getElementById('ans-btn1');
+var $ans2 = document.getElementById('ans-btn2');
+var $ans3 = document.getElementById('ans-btn3');
 
 
 // array of questions
 var quizQuestions = [
   {
-    question1: "What is the term used for when water vapor is converted back into liquid water?",
+    question: "What is the term used for when water vapor is converted back into liquid water?",
     answers: {
        a: "Evaporation",
        b: "Condensation",
        c: "Precipitation"
-    },
-    correctAnswer: "b"
+    }
   },
   {
-    question2: "What destructive storm forms over the ocean and then makes landfall in coastal states?",
+    question: "What destructive storm forms over the ocean and then makes landfall in coastal states?",
     answers: {
        a: "Hurricane",
        b: "Tornado",
        c: "Squall"
-    },
-    correctAnswer: "a"
+    }
   },
   {
-    question3: "What is the type of storm that spawns a funnel shaped cloud and very strong winds?",
+    question: "What is the type of storm that spawns a funnel shaped cloud and very strong winds?",
     answers: {
        a: "Snow storm",
        b: "Dust-devil",
        c: "Tornado"
-    },
-    correctAnswer: "c"
+    }
   },
   {
-    question4: "What type of cloud is most common and generally doesn't bring rain?",
+    question: "What type of cloud is most common and generally doesn't bring rain?",
     answers: {
        a: "Cumulus",
        b: "Cirrus",
        c: "Stratus"
-    },
-    correctAnswer: "a"
+    }
   },
   {
-    question5: "What type of destructive storm is generally formed in the winter and usually brings high winds and white-out conditions?",
+    question: "What type of destructive storm is generally formed in the winter and usually brings high winds and white-out conditions?",
     answers: {
        a: "Squall",
        b: "Blizzard",
        c: "Monsoon"
-    },
-    correctAnswer: "b"
+    }
   },
   {
-    question6: "What comes first: lightning or thunder?",
+    question: "What comes first: lightning or thunder?",
     answers: {
       a: "lightning",
       b: "thunder"
-    },  
+    }
   }    
 ];
 
-// Score at start of quiz
-var score = 0;
+function questionOne() {
+  $currentQuestion.textContent = JSON.stringify(quizQuestions[0].question);
+  $ans1.textContent = JSON.stringify(quizQuestions[0].answers.a);
+  $ans2.textContent = JSON.stringify(quizQuestions[0].answers.b);
+  $ans3.textContent = JSON.stringify(quizQuestions[0].answers.c);
+  
+  $ans1.onclick = function() {
+    score++;
+    console.log("correct!");
+    console.log(score);
+    //questionTwo();
+  }
+
+  $ans2.onclick = function() {
+    timeLeft -= 5;
+    console.log("INCORRECT!");
+    console.log(score);
+  } 
+
+  $ans3.onclick = function() {
+    timeLeft -= 5;
+    console.log("INCORRECT!");
+    console.log(score);
+  } 
+}
+
+function questionTwo() {
+  $currentQuestion.textContent = JSON.stringify(quizQuestions[1].question);
+  $ans1.textContent = JSON.stringify(quizQuestions[1].answers.a);
+  $ans2.textContent = JSON.stringify(quizQuestions[1].answers.b);
+  $ans3.textContent = JSON.stringify(quizQuestions[1].answers.c);
+  
+  $ans1.onclick = function() {
+    score++;
+    console.log("correct!");
+    console.log(score);
+    
+  }
+
+  $ans2.onclick = function() {
+    timeLeft -= 5;
+    console.log("INCORRECT!");
+    console.log(score);
+    
+  } 
+
+  $ans3.onclick = function() {
+    timeLeft -= 5;
+    console.log("INCORRECT!");
+    console.log(score);
+    
+  } 
+}
+
+var questionArray = [];
+questionArray.push(questionOne);
+questionArray.push(questionTwo);
 
 
 
@@ -78,13 +134,28 @@ function timer() {
 }
 
 function startQuiz() {
-  $question = document.getElementById("question");
-  $question.textContent = ;
-  console.log($question);
+  timer();
+
+  for (var i = 0; questionArray.length; ++i) {
+    questionArray[i]();
+  }
+  
+  
+  
+  
+
+
+
+  
+  
 
 
 }
 
 
 
-document.getElementById("btn").addEventListener("click", startQuiz);
+$startBtn.addEventListener("click", function(){
+  $startBtn.remove();
+
+  startQuiz();
+});
